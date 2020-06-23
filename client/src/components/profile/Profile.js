982aaa5f0d807164aa4264395e9bc7d9f59c92a8
sank8dalvi <sanket.dalvi.ssd@gmail.com>
2020-06-24 01:46:57 +0530

    Add profile top of the user frontend

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -2,6 +2,9 @@ import React, { Component, useEffect } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import { getProfileById } from '../../actions/profile'
+import Spinner from '../layout/Spinner'
+import { Link } from 'react-router-dom'
+import ProfileTop from './ProfileTop'
 
 const Profile = ({
     getProfileById,
@@ -12,13 +15,28 @@ const Profile = ({
 
     useEffect(() => {
         getProfileById(match.params.id);
-    }, [getProfileById])
+    }, [getProfileById, match.params.id])
 
     return (
-        <div>
-            Profile
-        </div>
-    )
+			<React.Fragment>
+				{profile === null || loading ? (
+					<Spinner />
+				) : (
+					<React.Fragment>
+						<Link to="/profiles" className="btn btn-light">
+							Back to Profiles
+						</Link>
+						{auth.isAuthenticated &&
+							auth.loading === false &&
+							auth.user._id === profile.user._id && 
+                            <Link to='/edit-profile' className='btn btn-dark'>Edit Profile</Link>}
+                            <div className="profile-grid my-1">
+                                <ProfileTop profile={profile} />
+                            </div>
+					</React.Fragment>
+				)}
+			</React.Fragment>
+		);
 }
 
 Profile.propTypes = {
