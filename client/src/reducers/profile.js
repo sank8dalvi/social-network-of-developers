a272a68dedc1ca444a0bdd5278f74bb47be7e0a5
sank8dalvi <sanket.dalvi.ssd@gmail.com>
2020-06-23 16:12:06 +0530

    Fix profile loading error

--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -42,7 +42,6 @@ export default function(state=initialState, action){
                 ...state,
                 profile: null,
                 repos: [],
-                loading: false,
             }
         default:
             return state
